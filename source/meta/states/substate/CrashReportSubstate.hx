0ffe5cccbacc93a6a3546d2504fb58f80c99c958
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-12-22 13:16:36 +0800

    Update CrashReportSubstate.hx

--- source/meta/states/substate/CrashReportSubstate.hx
+++ source/meta/states/substate/CrashReportSubstate.hx
@@ -50,7 +50,7 @@ class CrashReportSubstate extends FlxState {
 		report.borderSize = 1.5;
 		add(report);
 
-		underText = new FlxText(0, FlxG.height - 64, FlxG.width, "Press SPACE to return to the Menu Screen.");
+		underText = new FlxText(0, FlxG.height - 64, FlxG.width, "Touch screen to return to the Menu Screen.");
 		underText.setFormat(Paths.font('vcr.ttf'), 24, 0xFFFFFFFF, CENTER, OUTLINE, 0xFF000000);
 		underText.y = FlxG.height - underText.height - 16;
 		underText.borderSize = 1;
@@ -65,8 +65,14 @@ class CrashReportSubstate extends FlxState {
 	override function update(elapsed:Float):Void {
 		super.update(elapsed);
 
-		if (FlxG.keys.justPressed.SPACE){
+		#if mobile
+		for (touch in FlxG.touches.list)
+				{
+		if (touch.justPressed){
 			Init.SwitchToPrimaryMenu();
-        }
+		}
+
+		#end
+				}
 	}
 }
